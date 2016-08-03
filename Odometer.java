298b06ec27d09887f994910159beddd3bfbcfdff
Rajesh <srajesh@paypalcorp.com>
2016-08-03 17:20:11 +0530

    Initial

---
+++ Odometer.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Odometer {
+	
+	
+	public static ArrayList<Integer> getNextReading(){
+	
+		return null;
+	}
+	
+	public static ArrayList<Integer> getPrevReading(){
+		
+		return null;
+	}
+	
+	public static Integer getDistance(ArrayList<Integer> num1, ArrayList<Integer> num2){
+	
+		return null;
+	}
+	
+	public static ArrayList<Integer> getNextNthReading(ArrayList<Integer> num){
+		
+		return null;
+	}
+	
+	public static ArrayList<Integer> getPrevNthReading(ArrayList<Integer> num){
+		
+		return null;
+	}
+	
+	public static ArrayList<Integer> getLargestNum(Integer numDigit){
+		
+		return null;
+	}
+	
+	public static ArrayList<Integer> getSmallestNum(Integer numDigit){
+		
+		return null;
+	}
+	
+	public static boolean isValidOdoNumber(ArrayList<Integer> num){
+		
+		return false;
+	}
+	
+	public static ArrayList<Integer> convertToArrayList(int num){
+		
+		return null;
+	}
+	
+	public static Integer convertToNum(ArrayList<Integer> num){
+		
+		return null;
+	}
+
+}
